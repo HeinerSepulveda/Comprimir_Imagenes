33d8d676b6e35e2049c7b33845dc69917b3d3b80
Heiner Sepulveda <heinse.hs@gmail.com>
2023-01-05 20:38:44 -0500

    Cambios ruta imagenes

--- subir.php
+++ subir.php
@@ -5,7 +5,7 @@ if(isset($_POST) && ($_POST['Accion'] =="subirArchivo")){
 	$imagenesCargadas = $_FILES;
 	foreach ($imagenesCargadas as $imagen) {
 		$ArchivoOrigen = $imagen['tmp_name'];
-		move_uploaded_file($ArchivoOrigen, 'images/'.$imagen['name']);
+		move_uploaded_file($ArchivoOrigen, 'imagenes/'.$imagen['name']);
 	}
 
 }
